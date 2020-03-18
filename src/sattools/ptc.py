afb57549c1a6ba567f41ced341c21f113cbd362e
Gerrit Holl <gerrit.holl@dwd.de>
2020-03-18 15:54:59 +0100

    Add functions to collect all comps and mods
    Add two functions to collect some or all composites and modifiers and
    add them to a scene.  This as a workaround for satpy issue 784.

--- src/sattools/ptc.py
+++ src/sattools/ptc.py
@@ -1,6 +1,8 @@
 """Utilities related to pytroll configuration
 """
 
+import satpy.composites
+import satpy.config
 import pkg_resources
 import pyresample
 
@@ -15,3 +17,27 @@ def get_all_areas(packages):
         areas = pyresample.area_config.load_area(fn)
         D.update({ar.area_id: ar for ar in areas})
     return D
+
+
+def add_pkg_comps_mods(scn, pkg, sensors=["abi"]):
+    """Add composites and modifiers to sensor dictionary for one package
+
+    Awaiting a solution to the issue at
+    https://github.com/pytroll/satpy/issues/784 for a proper plugin system,
+    this function will take an existing Scene object and makes it aware of
+    composites and modifiers
+    """
+    p = pkg_resources.resource_filename(pkg, "etc/")
+    cpl = satpy.composites.CompositorLoader(p)
+    (comps, mods) = cpl.load_compositors(sensors)
+    satpy.config.recursive_dict_update(scn.dep_tree.composites, comps)
+    satpy.config.recursive_dict_update(scn.dep_tree.mods, mods)
+
+
+def add_all_pkg_comps_mods(scn, pkgs, sensors=["abi"]):
+    """Add composites and modifiers to sensor dictionary for all packages
+
+    See :func:`add_pkg_comps_mods`.
+    """
+    for pkg in pkgs:
+        add_pkg_comps_mods(scn, pkg, sensors)
--- tests/test_ptc.py
+++ tests/test_ptc.py
@@ -1,23 +1,40 @@
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 
-@patch("pyresample.area_config.load_area", autospec=True)
-@patch("pkg_resources.resource_filename", autospec=True)
-def test_get_areas(prf, pac):
+def test_get_areas():
     # need to mock pkg_resources.resource_filename
     # and pyresample.area_config.load_area
     # such that I get a list of areas
     import pyresample.geometry
     import sattools.ptc
-    prf.return_value = "/dev/null"
     ad = pyresample.geometry.AreaDefinition(
             "shrubbery", "it is a good shrubbery", "shrub",
             {'ellps': 'WGS84', 'lat_0': '0', 'lat_ts': '0', 'lon_0': '0',
              'no_defs': 'None', 'proj': 'eqc', 'type': 'crs', 'units': 'm',
              'x_0': 0, 'y_0': 0},
             750, 300, (2500000, 4000000, 3000000, 40000000))
-    pac.return_value = [ad]
-    D = sattools.ptc.get_all_areas(["tofu", "tempeh"])
-    assert prf.call_count == 2
-    assert pac.call_count == 2
-    assert D == {"shrubbery": ad}
+    with patch("pyresample.area_config.load_area", autospec=True) as pal, \
+         patch("pkg_resources.resource_filename", autospec=True) as prf:
+        prf.return_value = "/dev/null"
+        pal.return_value = [ad]
+        D = sattools.ptc.get_all_areas(["tofu", "tempeh"])
+        assert prf.call_count == 2
+        assert pal.call_count == 2
+        assert D == {"shrubbery": ad}
+
+
+def test_add_pkg():
+    import sattools.ptc
+    scn = MagicMock()
+    with patch("satpy.composites.CompositorLoader", autospec=True) as scC, \
+         patch("pkg_resources.resource_filename", autospec=True) as prf:
+        prf.return_value = "/dev/null"
+        scC.return_value.load_compositors.return_value = ({}, {})
+        sattools.ptc.add_pkg_comps_mods(scn, "apple", ["tomato"])
+
+
+def test_add_pkgs():
+    import sattools.ptc
+    scn = MagicMock()
+    with patch("sattools.ptc.add_all_pkg_comps_mods", autospec=True) as spa:
+        sattools.ptc.add_all_pkg_comps_mods(scn, ["apple", "strawberry"], ["tomato"])
