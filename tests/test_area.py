7216cf147a09a5ac570bf662d7fbfa4e8ce5880d
Gerrit Holl <gerrit.holl@dwd.de>
2020-07-24 11:42:06 +0200

    Add test for situations of ValueError

--- tests/test_area.py
+++ tests/test_area.py
@@ -1,3 +1,4 @@
+import pytest
 from numpy.testing import assert_allclose
 
 
@@ -7,6 +8,8 @@ def test_join_areadefs():
     proj_dict = {'proj': 'geos', 'sweep': 'x', 'lon_0': 0, 'h': 35786023,
 		 'x_0': 0, 'y_0': 0, 'ellps': 'GRS80', 'units': 'm',
 		 'no_defs': None, 'type': 'crs'}
+    proj_dict_alt = {'proj': 'laea', 'lat_0': -90, 'lon_0': 0, 'a': 6371228.0,
+                     'units': 'm' }
 
     ar1 = create_area_def(
             "test-area",
@@ -29,5 +32,23 @@ def test_join_areadefs():
             area_extent=[20, 0, 120, 100],
             shape=(10, 10))
 
+    ar4 = create_area_def(
+            "test-area",
+            projection=proj_dict_alt,
+            units="m",
+            area_extent=[20, 0, 120, 100],
+            shape=(10, 10))
+
+    ar5 = create_area_def(
+            "test-area",
+            projection=proj_dict,
+            units="m",
+            area_extent=[-50, -50, 50, 50],
+            shape=(100, 100))
+
     ar_joined = join_areadefs(ar1, ar2, ar3)
     assert_allclose(ar_joined.area_extent, [0, 0, 120, 140])
+    with pytest.raises(ValueError):
+        join_areadefs(ar3, ar4)
+    with pytest.raises(ValueError):
+        join_areadefs(ar3, ar5)
